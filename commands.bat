9ccd9a09e1bc766f3f73d237ef874955d24e8545
Muztahid-2020331021 <sajid191693@gmail.com>
2026-05-10 19:15:57 +0600

    Added EDA project on the Titanic dataset

--- commands.bat
+++ commands.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "page-footer fixed and contact info added"
+git commit -m "Added EDA project on the Titanic dataset"
 git push
 quarto publish gh-pages --no-prompt --no-browser
