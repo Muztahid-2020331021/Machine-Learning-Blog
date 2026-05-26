94831ddafada3c666955679aca2911047f1e2a01
Muztahid-2020331021 <sajid191693@gmail.com>
2026-05-27 01:24:02 +0600

    Updated Machine Learning pipeline

--- commands.bat
+++ commands.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Added test-train split & column transformer and Machine Learning pipeline"
+git commit -m "Updated Machine Learning pipeline"
 git push origin main
 quarto publish gh-pages --no-prompt --no-browser
