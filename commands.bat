4caebe1347f7603b4fdf12c06f39f836935e612b
Muztahid-2020331021 <sajid191693@gmail.com>
2026-06-19 00:37:46 +0600

    Fixed Mermaid Chart

--- commands.bat
+++ commands.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Added linear regression in-depth math intuition article"
+git commit -m "Fixed Mermaid Chart"
 git push origin main
 quarto publish gh-pages --no-prompt --no-browser
